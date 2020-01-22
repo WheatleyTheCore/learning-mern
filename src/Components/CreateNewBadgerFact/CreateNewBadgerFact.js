61c3ec316d5f324f263a60cdb3739bbabf65029a
WheatleyTheCore <alex.colwell@icloud.com>
2020-01-22 18:20:33 -0500

    connected backend to frontend with axios

--- src/Components/CreateNewBadgerFact/CreateNewBadgerFact.js
+++ src/Components/CreateNewBadgerFact/CreateNewBadgerFact.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import CKEditor from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import axios from 'axios'
 
 let CreateNewBadgerFact = () => {
     const [data, setData] = useState('');
@@ -10,13 +11,13 @@ let CreateNewBadgerFact = () => {
             <h3>Badger Facts Please!</h3>
             <CKEditor
                 editor={ClassicEditor}
-                data="<p>Hello from CKEditor 5!</p>"
+                data="Hello from CKEditor 5!"
                 onInit={editor => {
                     // You can store the "editor" and use when it is needed.
                     console.log('Editor is ready to use!', editor);
                 }}
                 onChange={(event, editor) => {
-                    const editorData = editor.getData();
+                    const editorData = editor.getData()
                     setData(editorData)
                     console.log(data)
                 }}
@@ -28,6 +29,14 @@ let CreateNewBadgerFact = () => {
                 }}
             />
             <button className="btn btn-primary" onClick={() => {
+
+                const NewBadgerFact = {
+                    badgerFact_data: data
+                }
+
+                axios.post("http://localhost:4000/badgerFacts/add", NewBadgerFact)
+                    .then(res => console.log(res.data))
+
                 console.log("WOOOO")
             }}>Done!</button>
         </div>
--- src/Components/ListOfFacts/ListOfFacts.js
+++ src/Components/ListOfFacts/ListOfFacts.js
@@ -1,9 +1,41 @@
 import React from 'react'
+import axios from 'axios'
 
-let ListOfFacts = () => {
-    return (
-        <h3>I'm a list</h3>
-    )
+class ListOfFacts extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            facts: []
+        }
+
+        
+    }
+
+    componentDidMount() {
+        axios.get("http://localhost:4000/badgerFacts/")
+            .then(response => {
+                this.setState({facts: response.data})
+                console.log(response.data)
+            })
+            .catch(err => {
+                console.error(err)
+            })
+        
+    }
+    
+    render() {
+        return (
+            this.state.facts.map(fact => {
+                return (
+                    <div>
+                       {fact.badgerFact_data}
+                       <hr />
+                    </div>
+                )
+            })
+        )
+    } 
 }
 
 export default ListOfFacts
